b5b1ba285491cf3f0baf8043e8723d99e38dc8ff
Raghu7331 <raghuram7331@gmail.com>
2025-11-03 15:08:14 +0530

    Remove Home link from Login and Signup page navigation

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -208,7 +208,7 @@ export default function Signup() {
   return (
     <div style={page}>
       <header style={header}>
-        <Link to="/" style={{ ...brand, textDecoration: "none" }}>Career Navigator</Link>
+        <div style={{ ...brand, textDecoration: "none" }}>Career Navigator</div>
         <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
           <nav style={nav}>
             <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
